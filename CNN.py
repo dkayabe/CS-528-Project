212181a506d55fd46940aaad129014fa664ec5d4
dkayabe <danlka1903@gmail.com>
2023-04-23 17:11:25 -0500

    04/23/2023
    Updated CNN sequential model working (no vulnerability detection or input yet)

--- CNN.py
+++ CNN.py
@@ -1,1 +1,34 @@
 #Testing Keras
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+CNN = Sequential(name="Sequential_CNN")
+
+CNN.add(Conv2D(16, kernel_size=(3, 3),
+               strides=(2, 2), padding="same",
+               activation="relu", input_shape=(28, 28, 1)))
+
+CNN.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2),
+                     padding="valid"))
+
+# Add another pair of Conv2D and MaxPooling2D for more model depth,
+# followed by the flatten and multiple dense layers
+
+CNN.add(Conv2D(32, kernel_size=(3, 3),
+               strides=(2, 2), padding="same",
+               activation="relu"))
+
+CNN.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2),
+                     padding="valid"))
+
+CNN.add(Flatten())
+
+CNN.add(Dense(64, activation='relu'))
+CNN.add(Dense(32, activation='relu'))
+CNN.add(Dense(10, activation='softmax'))
+
+CNN.summary()
